debdd3529a3402d5385d3b0a3831212bb56030e6
Zingirdas <zingirdas@googlemail.com>
2024-04-08 23:54:46 +0300

    Updat_klaida

--- salis.js
+++ salis.js
@@ -1,4 +1,3 @@
-
 const saliesPavadinimas = document.getElementById("pavadinimas");
 const ieskotiPavadinimo = document.getElementById("ieskotiPav");
 const btnSearch = document.getElementById("ieskoti");
@@ -8,29 +7,51 @@ const saliesValiuta = document.getElementById("valiuta");
 const saliestKalba = document.getElementById("kalba");
 const saliesVeliava = document.getElementById("veliava");
 const saliesHerbas = document.getElementById("herbas");
-const salysKaimynes = document.getElementById("kaimynes")
+const salysKaimynes = document.getElementById("kaimynes");
+const loading = document.getElementById("loading");
+const pranesimas = document.getElementById("pranesimas");
+
+pranesimas.style.display = 'none';
 
 
-const surastiSaliPagalPavadinima = () => {
+fetch(`https://restcountries.com/v3.1/all?fields=name`)
+    .then((response) => {
+        loading.style.display = "block";
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        data.forEach((country) => {
+            const option = document.createElement("option");
+            option.textContent = country.name.common;
+            option.value = country.name.common;
+            ieskotiPavadinimo.appendChild(option);
+        });
+        loading.style.display = "none";
+
+    });
+
+function clearCountryInfo() {
+    saliesPavadinimas.innerHTML = '';
+    saliesPopuliacija.innerHTML = '';
+    saliesPlotas.innerHTML = '';
+    saliesValiuta.innerHTML = '';
+    saliestKalba.innerHTML = '';
+    saliesVeliava.innerHTML = '';
+    saliesHerbas.innerHTML = '';
+    salysKaimynes.innerHTML = '';
+}
+
+function searchCountries() {
     const name = ieskotiPavadinimo.value;
+    loading.style.display = "block";
     fetch(`https://restcountries.com/v3.1/name/${name}`)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            console.log(data);
-
-            saliesPavadinimas.innerHTML = '';
-            saliesPopuliacija.innerHTML = '';
-            saliesPlotas.innerHTML = '';
-            saliesValiuta.innerHTML = '';
-            saliestKalba.innerHTML = '';
-            saliesVeliava.innerHTML = '';
-            saliesHerbas.innerHTML = '';
-            salysKaimynes.innerHTML = '';
-
-            data.forEach(country => {
-
+            console.log("Valstybe", data);
+            data.forEach((country) => {
                 const pavadinimas = document.createElement("div");
                 pavadinimas.textContent = country.name.common;
                 saliesPavadinimas.appendChild(pavadinimas);
@@ -60,16 +81,51 @@ const surastiSaliPagalPavadinima = () => {
                 herbas.src = country.coatOfArms.png;
                 herbas.classList.add("coat-of-arms");
                 saliesHerbas.appendChild(herbas);
-
-                const kaimynes = document.createElement("div");
-                kaimynes.textContent = `${country.borders}`;
-                salysKaimynes.appendChild(kaimynes);
-
             });
+            loading.style.display = "none";
+        })
+        .catch((error) => {
+            console.log(typeof error);
+            console.log(`Klaida: ${error}`);
+            loading.style.display = "none";
+            if (error.name == "TypeError") {
+                pranesimas.innerHTML = `Klaida, serveris neveikia arba nėra interneto`;
+            }
+            pranesimas.style.display = 'block';
+        });
+
+}
+
+const surastiSaliPagalPavadinima1 = () => {
+    const name = ieskotiPavadinimo.value;
+    loading.style.display = "block";
+    fetch(`https://restcountries.com/v3.1/name/${name}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log("Valstybe");
+            return fetch(`https://restcountries.com/v3.1/alpha?codes=${data[0].borders.join(',')}`)
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            data.forEach((c) => {
+                const kaimyneDiv = document.createElement("div");
+                kaimyneDiv.textContent = `${c.name.common}:  (${c.population})`;
+                salysKaimynes.appendChild(kaimyneDiv);
+            })
+                .then((response) => {
+                    return response.json("Nera kaimynu")
+                })
+            loading.style.display = "none";
         })
 }
 
-btnSearch.onclick = surastiSaliPagalPavadinima;
-
-
-
+btnSearch.addEventListener("click", () => {
+    clearCountryInfo();
+    searchCountries();
+    surastiSaliPagalPavadinima1();
+});
